82a20cd70c3fbf2334f9e632caec08e76ba7d4ca
shakhan <7shakhan@gmail.com>
2026-04-13 15:10:33 +0500

    Remove `ImagePlaceholder` component and replace its usage across components with standard `<img>` tags; add new external and internal image assets.

--- src/components/Functionality.tsx
+++ src/components/Functionality.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './Functionality.module.css';
 import { Icon } from './Icon';
-import { ImagePlaceholder } from './ImagePlaceholder';
 
 export const Functionality: React.FC = () => {
   const { t } = useTranslation();
@@ -23,11 +22,10 @@ export const Functionality: React.FC = () => {
               <p className={styles.cardDesc}>{t('functionality.category.desc')}</p>
             </div>
             <div className={styles.cardImageWrapper}>
-              <ImagePlaceholder
+              <img
                 className={styles.cardImage}
                 src="https://lh3.googleusercontent.com/aida/ADBb0ugzihRhzgZ4S0yLgEVJ9gaMDFidvuv2OwZdqg-Vbwbo_K6zoHVrn5Le0znAcTdPAvAWmdzy98NimtO57z2zWId6JHoLuPIpobblV2_pfWOslnLloxN6ilISzlbGi_L-q89TVgYLvAEUZ8wkM4EYDCm11MPgWrNDm2wS9jtyBLOPe93euTx5_pttd7yrWma6bKHrk1zMWnta81cv6QmH2BuZn103M9wpHxwY-HQy-uqK2UFxj1DE8xo_HrISAJhn3ZohChJSMbiCcYs"
                 alt="Analytics by category"
-                aspectRatio="video"
               />
             </div>
           </div>
@@ -39,11 +37,10 @@ export const Functionality: React.FC = () => {
               <p className={styles.cardDesc}>{t('functionality.calculator.desc')}</p>
             </div>
             <div className={styles.cardImageWrapper}>
-              <ImagePlaceholder
+              <img
                 className={styles.cardImage}
                 src="https://lh3.googleusercontent.com/aida/ADBb0ui1rVLuaFdoWwK9nk00EPscKigmk_VKdN4q95CfNkH6tCVCyJqNebanhIeS90b9zcyC218HVWnZrLDfPboXEGBbLablWZBeZSS8fuKBc3k8CN7f0OhW-_QGt2miPRYbg8YteDe2USVdRXddWgQOLmk-lYL7ZFDfvknwc8VPFfujdRJHwVycByixrWUTz9cCuFyclh4l1uZH3HzcNTg4Kl4F3FxQ8MO4kmPXoZx3_DoIlaEq-1xuZZuahQKf3n_6UUCQRmy0FKCosko"
                 alt="Profitability calculator"
-                aspectRatio="video"
               />
             </div>
           </div>
--- src/components/InternalAnalytics/InternalHero.tsx
+++ src/components/InternalAnalytics/InternalHero.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import styles from './InternalHero.module.css';
-import { ImagePlaceholder } from '../ImagePlaceholder';
 
 export const InternalHero: React.FC = () => {
   const { t } = useTranslation();
@@ -31,10 +30,9 @@ export const InternalHero: React.FC = () => {
           <div className={styles.imageWrapper}>
             <div className={styles.imageGlow}></div>
             <div className={styles.imageContainer}>
-              <ImagePlaceholder
-                src="https://lh3.googleusercontent.com/aida/ADBb0uj0TZj2sQGjTAs1i_40rR6Lvj_2vaLrmHqZrXkCNqzuHy5Cs9ThYy1E_4BoCrlF_9SV0gvJkuFeIN-KLXFirCmWyqL_XCHV-ELXvicL9RJAu6Uia1uux7tktvZ-LwkFLauoQcfn5Fj2BGYEjAnkKmwYpq_YaFK2mD1DzrYUc9zpVZTYn0i9r_Dsil0EKhoV1joR8aB2jND1v8tgJ5P7HeRUcbFcMYVXwZxX5S1YJ1AfUmSjxYc42sDBJ5V2afftY3hKJgWeYjNdzIU"
+              <img
+                src="/internal.png"
                 alt="Internal Analytics Infographic"
-                aspectRatio="video"
               />
             </div>
           </div>
